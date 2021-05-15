ddde9ed00b3eee8178e58f8db6438fd0c915af77
Christoph Witzko <github@christophwitzko.com>
2021-05-15 22:37:16 +0200

    fix: unbufferd signal channel

--- service/cmd/gosship/main.go
+++ service/cmd/gosship/main.go
@@ -26,7 +26,7 @@ func main() {
 	log.Println("resetting database...")
 	db.ResetExceptConfig()
 
-	c := make(chan os.Signal)
+	c := make(chan os.Signal, 1)
 	signal.Notify(c, os.Interrupt, syscall.SIGTERM)
 	go func() {
 		<-c
